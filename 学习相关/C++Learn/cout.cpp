9e06e6d113b110a8b4f656202933daaf57267283
student <b2993166234@outlook.com>
2026-07-11 17:22:31 +0800

    C++第一天学习

---
+++ 学习相关/C++Learn/cout.cpp
@@ -0,0 +1,16 @@
+#include "iostream"
+using namespace std;
+
+int main()
+{
+	cout << "Hello, World!" << endl;
+	cout << "I love C++" << endl;
+
+	cout << 10 << endl;
+	
+	cout << "I an 10 " << "years old" << endl;
+
+	cout << "If you want to learn IT, " << "come to itheima" << endl;
+
+	return 0;
+}
